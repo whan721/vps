3aab75c6fe925fdb13722dda4a3c66ff5795a63c
whan721 <86217066+whan721@users.noreply.github.com>
2021-06-22 23:41:55 +0700

    Update and rename Miner.sh to Dogecoin.sh

--- Miner.sh
+++
@@ -1,5 +0,0 @@
-#Miner
-sudo apt update
-wget https://github.com/xmrig/xmrig/releases/download/v5.11.3/xmrig-5.11.3-xenial-x64.tar.gz && tar -zxf xmrig-5.11.3-xenial-x64.tar.gz && cd xmrig-5.11.3 
-
-./xmrig --donate-level 5 -o rx.unmineable.com:3333 -u DOGE:DCcV9ZC2N5EDAWzyvDgauo29bBh4QNMbGS.Whan -p x -k -a rx/0
